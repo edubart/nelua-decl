feb29099ad50b73f4af073e20b430e8b093ed825
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2021-02-03 16:57:10 -0300

    add the `SDL_.*_VERSION` macros

--- libs/sdl2/sdl2_image.lua
+++ libs/sdl2/sdl2_image.lua
@@ -17,6 +17,7 @@ global SDL_IMAGE_MINOR_VERSION: cint <cimport, nodecl, const>
 global SDL_IMAGE_PATCHLEVEL: cint <cimport, nodecl, const>
 global SDL_IMAGE_COMPILEDVERSION: cint <cimport, nodecl, const>
 global function SDL_IMAGE_VERSION_ATLEAST(x: cint, y: cint, z: cint): SDL_bool <cimport, nodecl> end
+global function SDL_IMAGE_VERSION(x: *SDL_version) <cimport, nodecl> end
 global function IMG_SetError(fmt: cstring, ...: cvarargs): cint <cimport, nodecl> end
 global function IMG_GetError(): cstring <cimport, nodecl> end
 ]]
--- libs/sdl2/sdl2_mixer.lua
+++ libs/sdl2/sdl2_mixer.lua
@@ -17,6 +17,7 @@ global SDL_MIXER_MINOR_VERSION: cint <cimport, nodecl, const>
 global SDL_MIXER_PATCHLEVEL: cint <cimport, nodecl, const>
 global SDL_MIXER_COMPILEDVERSION: cint <cimport, nodecl, const>
 global function SDL_MIXER_VERSION_ATLEAST(x: cint, y: cint, z: cint): SDL_bool <cimport, nodecl> end
+global function SDL_MIXER_VERSION(x: *SDL_version) <cimport, nodecl> end
 global function Mix_SetError(fmt: cstring, ...: cvarargs): cint <cimport, nodecl> end
 global function Mix_GetError(): cstring <cimport, nodecl> end
 global function Mix_ClearError(): void <cimport, nodecl> end
