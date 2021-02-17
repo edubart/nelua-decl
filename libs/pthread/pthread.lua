1b4737f9ba07b84632624e6b7ce91467981d578a
Eduardo Bart <edub4rt@gmail.com>
2021-02-17 19:25:33 -0300

    Update to latest Nelua and libraries

--- libs/pthread/pthread.lua
+++ libs/pthread/pthread.lua
@@ -3,6 +3,7 @@ local nldecl = require 'nldecl'
 nldecl.include_names = {
   '^__pthread',
   '^pthread',
+  __cancel_jmp_buf_tag = true,
   sched_param = true,
   timespec = true,
   pthread_t = true,
