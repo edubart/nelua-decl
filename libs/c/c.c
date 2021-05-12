4c88aadda024b4e42d9741c7775e9f68c429e8fd
Eduardo Bart <edub4rt@gmail.com>
2021-05-12 07:56:43 -0300

    Import C11 functions only for C library

--- libs/c/c.c
+++ libs/c/c.c
@@ -1,11 +1,9 @@
 #include <assert.h>      // Conditionally compiled macro that compares its argument to zero
-// #include <complex.h>     // (C99) Complex number arithmetic
 #include <ctype.h>       // Functions to determine the type contained in character data
 #include <errno.h>       // Macros reporting error conditions
 #include <fenv.h>        // (C99) Floating-point environment
 #include <float.h>       // Limits of float types
 #include <inttypes.h>    // (C99) Format conversion of integer types
-#include <iso646.h>      // (C95) Alternative operator spellings
 #include <limits.h>      // Sizes of basic types
 #include <locale.h>      // Localization utilities
 #include <math.h>        // Common mathematics functions
@@ -13,7 +11,6 @@
 #include <signal.h>      // Signal handling
 #include <stdalign.h>    // (C11) alignas and alignof convenience macros
 #include <stdarg.h>      // Variable arguments
-#include <stdatomic.h>   // (C11) Atomic types
 #include <stdbool.h>     // (C99) Macros for boolean type
 #include <stddef.h>      // Common macro definitions
 #include <stdint.h>      // (C99) Fixed-width integer types
@@ -21,9 +18,13 @@
 #include <stdlib.h>      // General utilities: memory management, program utilities, string conversions, random numbers
 #include <stdnoreturn.h> // (C11) noreturn convenience macros
 #include <string.h>      // String handling
-// #include <tgmath.h>      // (C99) Type-generic math (macros wrapping math.h and complex.h)
-#include <threads.h>     // (C11) Thread library
 #include <time.h>        // Time/date utilities
 #include <uchar.h>       // (C11) UTF-16 and UTF-32 character utilities
 #include <wchar.h>       // (C95) Extended multibyte and wide character utilities
 #include <wctype.h>      // (C95) Functions to determine the type contained in wide character data
+
+// #include <complex.h>     // (C99) Complex number arithmetic
+// #include <iso646.h>      // (C95) Alternative operator spellings
+// #include <stdatomic.h>   // (C11) Atomic types
+// #include <tgmath.h>      // (C99) Type-generic math (macros wrapping math.h and complex.h)
+// #include <threads.h>     // (C11) Thread library
