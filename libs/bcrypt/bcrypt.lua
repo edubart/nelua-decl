8a72bd2c3bcd52e48a7499a94947a3014ee66e1d
Elperson <40611324+Elpersonn@users.noreply.github.com>
2021-07-21 22:09:49 +0200

    Update bcrypt.lua

--- libs/bcrypt/bcrypt.lua
+++ libs/bcrypt/bcrypt.lua
@@ -3,5 +3,9 @@ local nldecl = require('nldecl')
 nldecl.prepend_code = [=[
 ##[[
 cinclude "bcrypt.h"
+cfile "bcrypt.c"
+cfile "crypt_blowfish/crypt_gensalt.c"
+cfile "crypt_blowfish/wrapper.c"
+cfile "crypt_blowfish/crypt_blowfish.c"
 ]]
 ]=]
