ac6e3b2d86f93f0f383e30b6ae5106a3ba644600
codehz <codehz@outlook.com>
2021-09-01 14:14:18 +0800

    fix test, add to readme

--- libs/tree-sitter/tree-sitter.lua
+++ libs/tree-sitter/tree-sitter.lua
@@ -2,4 +2,14 @@ local nldecl = require 'nldecl'
 
 nldecl.include_names = {
   '^TS',
+  '^ts_',
+  FILE = true,
 }
+
+nldecl.prepend_code = [=[
+##[[
+cinclude 'tree_sitter/api.h'
+cinclude 'tree_sitter/parser.h'
+linklib 'tree-sitter'
+]]
+]=]
